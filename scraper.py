53e5d2fe709b4aed35252a9a32533cc4bca1ccbc
Allison Liu <allison23liu@berkeley.edu>
2023-04-12 22:53:45 -0700

    fixed absolute xpaths

--- scraper.py
+++ scraper.py
@@ -10,8 +10,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-my_username = "yashwang23"
-my_password = "testingwhatnot"
+my_username = "yourinput"
+my_password = "yourinput"
 # usernames = [ 
 #     "utileschic", "illustratorjake", "tuzineko", "dollipoppette", "kittenslittlewonders", "kawaiipaperdream", 
 #     "kawaiigirlz.1", "lacasitadelasternuritas", "riversidekawaiishop", "katrinasdreamsshop", "holleyteatime", "polymernai", "luckyrainbowboutique", "divinegracehealing", "espa.cio120", "artemkemirov", 
@@ -31,7 +31,7 @@ my_password = "testingwhatnot"
 #     "hiro.sep10ber", "bombo_maka.shop", "bluebearyco", "ubekeen", "kwaiishup_", "cautiivate",
 #     "macarons_and_stilettos", "catmeilingcreations", "kumacolv", "sanriotyler", "dollipop_sweet_shoppe",
 #     "anime.healing"]
-usernames = ["utileschic", "illustratorjake", "tuzineko", "dollipoppette", "kittenslittlewonders", "kawaiipaperdream"]
+usernames = ["allison23liu"]
 messages = ["Hi! We’re running marketing and outreach tests on behalf of our company. Please disregard this message and have a great day!"]
 
 #browser = webdriver.Chrome('chromedriver_mac_arm64')
@@ -41,12 +41,16 @@ def auth(username, password):
         browser.get('https://instagram.com')
         time.sleep(random.randrange(2,4))
 
-        #wait for page to load
-        # WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.NAME, "name")))
-        # WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.NAME, "password")))
+        print("point 1: load website")
 
-        input_username = browser.find_element("name", 'username')
-        input_password = browser.find_element("name", 'password')
+        #wait for page to load
+        WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.NAME, "username")))
+        WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.NAME, "password")))
+
+        input_username = browser.find_element(By.NAME, "username")
+        input_password = browser.find_element(By.NAME, "password")
+
+        print("point 2: found username & password entry location")
 
         input_username.send_keys(username)
         time.sleep(random.randrange(1,2))
@@ -54,6 +58,9 @@ def auth(username, password):
         time.sleep(random.randrange(1,2))
         input_password.send_keys(Keys.ENTER)
         time.sleep(random.randrange(1,2))
+
+        print("point 3: successfully logged in")
+
     except Exception as err:
         print(err)
         browser.quit()
@@ -62,42 +69,60 @@ def send_message(users, messages):
     try:
         #open messages tab
         time.sleep(random.randrange(2,4))
-        WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[5]/div/a/div"))).click()
+        
+        #ignore "Save login"
+        WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,'//div[text()="Not Now"]'))).click()
+        print("point 4: no to save login")
 
-        #browser.find_element("xpath", "/html/body/div[3]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[5]/div/a/div").click()
-        #browser.find_element("xpath", "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[5]/div/a/div").click()
+        #ignore "Turn on notifications"
         time.sleep(random.randrange(3,5))
-        #ignore notification
-        #browser.find_element("xpath", "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]").click()
-        WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]"))).click()
+        WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,'//button[text()="Not Now"]'))).click()
+        print("point 5: no to enable notifications")
+
+        #click on "Messages"
+        time.sleep(random.randrange(3,5))
+        WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/direct/inbox/']"))).click()
+        print("point 6: navigate to message page")
 
         for user in users:
 
             time.sleep(random.randrange(3,5))
-            #click on send message
-            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/section/div/div/div/div/div[1]/div[1]/div/div[3]/button"))).click()
+            #click on "Send message"
+            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Send Message"]'))).click()
             
-            time.sleep(random.randrange(10,12))
+            time.sleep(random.randrange(3,5))
             #search for user
-            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[1]/div/div[2]/input"))).send_keys(user)
+            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.NAME, "queryBox"))).send_keys(user)
+            
+            time.sleep(random.randrange(3,5))
+            print("point 7: type in username")
+            
 
-            #browser.find_element("xpath", "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[1]/div/div[2]/input").send_keys(user)
-            time.sleep(random.randrange(3,4))
+            # try: 
+            # #select user
+            #     WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,'//circle'))).click()
+            #     print("successfully selected user")
+            # except:
+            #     WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/button"))).click()
+            #     print("Failed to send to", user)
+            #     continue
+            
+            try:
+                #click circle locate_with(By.TAG_NAME, "input").below({By.ID: "email"})
+                WebDriverWait(browser, 15).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'svg[aria-label="Toggle selection"]'))).click()
+                time.sleep(random.randrange(1,3))
 
-            try: 
-            #select user
-                WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[2]/div[2]/div[1]/div[1]/div/div/div[3]/div/button"))).click()
-            except:
-                WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[2]/div/button"))).click()
-                print("Failed tp send to", user)
-                continue
-                
-            time.sleep(random.randrange(3,4))
-            #click next
-            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div[1]/div/div[3]/div/button"))).click()
-            time.sleep(random.randrange(3,4))
+                print("point 8: toggle selection")
+            except Exception as err:
+                print(err)
+                browser.quit()
+
+            #click Next
+            WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, '//div[text()="Next"]'))).click()
+            time.sleep(random.randrange(1,3))
+
             #send a random message in the text box
-            text_area = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[2]/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/section/div/div/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea")))
+            text_area = WebDriverWait(browser, 30).until(EC.element_to_be_clickable((By.XPATH, '//textarea')))
             text_area.send_keys(random.choice(messages))
             time.sleep(random.randrange(2,4))
             text_area.send_keys(Keys.ENTER)
@@ -115,6 +140,6 @@ def send_message(users, messages):
         print(err)
         browser.quit()
 
-# auth(my_username, my_password)
-# time.sleep(random.randrange(3,5))
-# send_message(usernames, messages)
+auth(my_username, my_password)
+time.sleep(random.randrange(3,5))
+send_message(usernames, messages)
