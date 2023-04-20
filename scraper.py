c54c4e6fcf9d5f19ad7b85f0b138838518d45df1
Ashok Devireddy <ashokrd2@gmail.com>
2023-04-19 19:47:57 -0700

    added window size

--- scraper.py
+++ scraper.py
@@ -15,9 +15,10 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--window-size=800,600")
 #chrome_options.add_argument("--disable-gpu")
 browser = webdriver.Chrome(executable_path= os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-print(browser.get_window_size())
+
 
 # options = Options()
 # options.add_argument("start-maximized")
