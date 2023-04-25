365d44898eb4193104adc0929d38c0542e1e55e2
Ashok Devireddy <ashokrd2@gmail.com>
2023-04-24 22:40:31 -0700

    changed to instabot

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from werkzeug.utils import secure_filename
 import os
 import csv
 from scraper import auth, send_message
-# import threading
+import threading
 from instabot import Bot
 import glob
 # import signal
@@ -47,15 +47,15 @@ def start():
         messages = ["Hi! We’re running marketing and outreach tests on behalf of our company. Please disregard this message and have a great day!"]
 
         #uncomment bellow code for selenium webdriver
-        auth(username, password)
-        time.sleep(random.randrange(3,5))
-        send_message(usernames, messages)
+        # auth(username, password)
+        # time.sleep(random.randrange(3,5))
+        # send_message(usernames, messages)
 
         #code for instabot
         # thread = threading.Thread(target=run_instabot, args=(username, password, ["allison23liu"], messages))
         # thread.start()
         # thread.join()
-        #send_msg(username, password, usernames, messages)
+        send_msg(username, password, usernames, messages)
 
 
         # do something with the username and csv file
